323d08559045c17eafb399ec792f16720e444695
SD <247677353+sdinformatica74@users.noreply.github.com>
2026-05-09 14:13:13 -0300

    Update cache name to handball-arbitro-v14

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — Regras de Jogo Handebol Indoor
 // SDInformática — Profª Synthia Duarte
 
-const CACHE_NAME = 'handball-arbitro-v13';
+const CACHE_NAME = 'handball-arbitro-v14';
 const ASSETS = [
   './',
   './index.html',
