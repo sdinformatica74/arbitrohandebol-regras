20d87b95c0f067f0a178720672091f04b1880d1e
SD <247677353+sdinformatica74@users.noreply.github.com>
2026-07-04 00:22:31 -0300

    Update CACHE_NAME to version 32

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — Regras de Jogo Handebol Indoor
 // SDInformática — Profª Synthia Duarte
 
-const CACHE_NAME = 'handball-arbitro-v31';
+const CACHE_NAME = 'handball-arbitro-v32';
 const ASSETS = [
   './',
   './index.html',
